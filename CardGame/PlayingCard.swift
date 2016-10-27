4098739d058b087611a61095a6053ff0f9f19fb7
Waffesrgud <gcor8853@740s-pl-132.local>
2016-10-27 08:52:35 -0600

    added more correct flesh to the getters and setters, and initialization exists again

--- CardGame/PlayingCard.swift
+++ CardGame/PlayingCard.swift
@@ -11,8 +11,51 @@ import UIKit
 class PlayingCard : Card
 {
     private var rank : Int
+    {
+        get
+        {
+            return self.rank
+        }
+        set(rank)
+        {
+            self.rank = rank
+        }
+    }
     private var suit : String
+    {
+        get
+        {
+            return self.suit
+        }
+        set(suit)
+        {
+            self.suit = suit
+        }
+    }
+    private var color : UIColor
+    {
+        get
+        {
+            return self.color
+        }
+        set(color)
+        {
+            self.color = color
+        }
+    }
+    private var frontImage : UIImage
+    {
+        get
+        {
+            return self.frontImage
+        }
+        set(frontImage)
+        {
+            self.frontImage = frontImage
+        }
+    }
 
+        
     func getRank() -> Int
     {
         return self.rank
@@ -23,13 +66,58 @@ class PlayingCard : Card
         return suit
     }
     
+    func getColor() -> UIColor
+    {
+        return color
+    }
+    
+    func getFrontImage() -> UIImage
+    {
+        return frontImage
+    }
+    
     func setRank(rank: Int) -> Void
     {
         self.rank = rank
+      /*  if (suit.equals("Spades") || suit.equals("Clubs"))
+        {
+            self.color = black ! ! ! come back to this later ! ! !
+        }   else if (suit.equals("Hearts") || suit.equals("Diamonds"))
+        {
+            self.color = red ! ! ! FIX THIS ONE TOO ! ! !
+        }
+      */
+
     }
     
-    func getSuit(suit: String) -> Void
+    func setSuit(suit: String) -> Void
     {
         self.suit = suit
+    }
+    
+    func setColor(color: UIColor) -> Void
+    {
+        self.color = color
+    }
+    
+    func setFrontImage(frontImage: UIImage) -> Void
+    {
+        self.frontImage = frontImage
+    }
+    
+    override init()
+    {
+        super.init()
+        frontImage = UIImage()
+        color = UIColor()
+        rank = 0
+        suit = ""
+    }
+    
+    override func toString() -> String
+    {
+    let description = "The card's rank is \(rank), is the color of \(color), and is of the \(suit) suit."
+        
+    return description
     }
 }
