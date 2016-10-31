bdc07d49880bc8e3cf58966fc606d02639c7d83b
Waffesrgud <gcor8853@740s-pl-132.local>
2016-10-31 09:56:15 -0600

    i tried

--- CardGame/PlayingCard.swift
+++ CardGame/PlayingCard.swift
@@ -142,13 +142,18 @@ class PlayingCard : Card
     return description
     }
     
-    class func cardRanks() -> [String]
+    class func validRanks() -> [String]
     {
         return ["??","A","2","3","4","5","6","7","8","9","10","J","Q","K"]
     }
     
     class func validSuits() -> [String]
     {
-        return [ "Spades", "Hearts", "Clubs", "Diamonds"]
+        return [ "♠️", "♥️", "♣️", "♦️"]
+    }
+    
+    class func maxRank() -> Int
+    {
+        return validRanks().count - 1
     }
 }
