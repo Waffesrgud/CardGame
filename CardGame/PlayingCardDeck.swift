b6d67446aef5a753ca05a9e5e57aaf4c6090ee24
Waffesrgud <gcor8853@740s-pl-132.local>
2016-10-31 08:40:54 -0600

    added framework to methods of playing card deck

--- CardGame/PlayingCardDeck.swift
+++ CardGame/PlayingCardDeck.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+
+class PlayingCardDeck : Deck
+    
+{
+    
+    override init()
+    {
+        
+    }
+    
+    func shuffleDeck() -> Void
+    {
+        
+    }
+    
+    func orderDeck() -> Void
+    {
+        
+    }
+}
